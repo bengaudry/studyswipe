25d7946a7b1c859db18964087b968eb82b2a8353
bengaudry <bengaudry@outlook.fr>
2025-01-14 12:44:03 +0100

    Styles: Updated deck page loader

--- src/app/deck/[deckid]/loading.tsx
+++ src/app/deck/[deckid]/loading.tsx
@@ -7,27 +7,25 @@ import { ChevronLeft, Play } from "react-feather";
 export default async function DeckPageLoader() {
   return (
     <div>
-      <Link href=".." className="text-neutral-400 flex items-center mb-3">
-        <ChevronLeft />
-        <span> Back</span>
-      </Link>
-      
-      <header className="flex justify-between items-center gap-4 mb-6">
-        <SkeletonLoader className="rounded-md">
-          <h1 className="text-2xl font-semibold">Title of whatever</h1>
-        </SkeletonLoader>
-        <Button
-          color="default"
-          size="sm"
-          isDisabled
-          disabled
-          startContent={<Play fill="#fff" size={18} />}
-        >
-          Launch
-        </Button>
+      <header className="flex flex-col gap-3 mb-6">
+        <div className="flex justify-between items-center gap-4">
+          <Link href=".." className="text-neutral-400 flex items-center mb-3">
+            <ChevronLeft />
+            <span> Back</span>
+          </Link>
+          <Button size="sm" startContent={<Play fill="#fff" size={18} />}>
+            Launch
+          </Button>
+        </div>
+        <div className="flex justify-between items-center gap-4">
+          <SkeletonLoader className="rounded-lg">
+            <h1 className="text-2xl font-semibold">Deck title</h1>
+          </SkeletonLoader>
+        </div>
       </header>
+
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
-        <NewCardModal deckid="" decktheme="neutral" />
+        <NewCardModal deckid={""} decktheme={"neutral"} />
       </div>
     </div>
   );
--- src/app/deck/[deckid]/page.tsx
+++ src/app/deck/[deckid]/page.tsx
@@ -7,6 +7,33 @@ import { ChevronLeft, Play } from "react-feather";
 import { clsx } from "clsx";
 import Link from "next/link";
 import { DeckOptionsDropdown } from "@/components/pages/deck/DeckOptionsDropdown";
+import { Deck } from "@prisma/client";
+import { wait } from "@/lib/time";
+
+export function DeckPageHeader({ deck }: { deck: Deck }) {
+  return (
+    <header className="flex flex-col gap-3 mb-6">
+      <div className="flex justify-between items-center gap-4">
+        <Link href=".." className="text-neutral-400 flex items-center mb-3">
+          <ChevronLeft />
+          <span> Back</span>
+        </Link>
+        <Button
+          color="primary"
+          size="sm"
+          className={clsx(`bg-${deck.theme}-500`)}
+          startContent={<Play fill="#fff" size={18} />}
+        >
+          Launch
+        </Button>
+      </div>
+      <div className="flex justify-between items-center gap-4">
+        <h1 className="text-2xl font-semibold">{deck.title}</h1>
+        <DeckOptionsDropdown deckId={deck.id} deckTitle={deck.title} />
+      </div>
+    </header>
+  );
+}
 
 export default async function DeckPage({
   params,
@@ -23,26 +50,8 @@ export default async function DeckPage({
 
   return (
     <div>
-      <header className="flex flex-col gap-3 mb-6">
-        <div className="flex justify-between items-center gap-4">
-          <Link href=".." className="text-neutral-400 flex items-center mb-3">
-            <ChevronLeft />
-            <span> Back</span>
-          </Link>
-          <Button
-            color="primary"
-            size="sm"
-            className={clsx(`bg-${deck.theme}-500`)}
-            startContent={<Play fill="#fff" size={18} />}
-          >
-            Launch
-          </Button>
-        </div>
-        <div className="flex justify-between items-center gap-4">
-          <h1 className="text-2xl font-semibold">{deck.title}</h1>
-          <DeckOptionsDropdown deckId={deck.id} deckTitle={deck.title} />
-        </div>
-      </header>
+      <DeckPageHeader deck={deck} />
+
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         <NewCardModal deckid={deck.id} decktheme={deck.theme} />
 
