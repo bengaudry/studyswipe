9579692f0cb97648584bde51f8cd9c86a04f6453
bengaudry <bengaudry@outlook.fr>
2025-01-14 13:56:19 +0100

    Fix: Launch button does not work on mobile

--- src/components/pages/deck/DeckPageHeader.tsx
+++ src/components/pages/deck/DeckPageHeader.tsx
@@ -5,22 +5,24 @@ import clsx from "clsx";
 import { Play } from "react-feather";
 import { DeckOptionsDropdown } from "./DeckOptionsDropdown";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export function DeckPageHeader({ deck }: { deck: Deck }) {
+  const { push } = useRouter();
+
   return (
     <header className="flex flex-col gap-3 mb-6">
       <div className="flex justify-between items-center gap-4">
         <BackButton />
-        <Link href={`/play/${deck.id}`}>
-          <Button
-            color="primary"
-            size="sm"
-            className={clsx(`bg-${deck.theme}-500`)}
-            startContent={<Play fill="#fff" size={18} />}
-          >
-            Launch
-          </Button>
-        </Link>
+        <Button
+          color="primary"
+          size="sm"
+          className={clsx(`bg-${deck.theme}-500`)}
+          onPress={() => push(`/play/${deck.id}`)}
+          startContent={<Play fill="#fff" size={18} />}
+        >
+          Launch
+        </Button>
       </div>
       <div className="flex justify-between items-center gap-4">
         <h1 className="text-2xl font-semibold">{deck.title}</h1>
