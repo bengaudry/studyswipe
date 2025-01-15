0566d9176ec97a75304307fdacf0202d41174091
bengaudry <bengaudry@outlook.fr>
2025-01-16 00:25:02 +0100

    Styles: ?

--- src/app/(layout)/(authguarded)/decks/loading.tsx
+++ src/app/(layout)/(authguarded)/decks/loading.tsx
@@ -5,7 +5,7 @@ export default function DecksLoader() {
   return (
     <div>
       <header className="flex items-center justify-between">
-        <h1 className="text-3xl font-semibold">My decks</h1>
+        <h1 className="text-3xl font-semibold">Collections</h1>
         <SkeletonLoader className="w-32 h-8 rounded-md" />
       </header>
       <div className="pt-6 grid place-content-center">
