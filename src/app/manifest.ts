96026f62f2a7ed18424ae6f5d7f831f174a06830
bengaudry <bengaudry@outlook.fr>
2025-01-21 19:35:42 +0100

    PWA

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,20 +1,21 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Studyswipe - Study with flashcards',
-    short_name: 'Studyswipe',
-    description: 'An app that allows you to study by gamifying your courses with flashcards.',
-    start_url: '/',
-    display: 'standalone',
-    background_color: '#ffffff',
-    theme_color: '#000000',
+    name: "Studyswipe - Study with flashcards",
+    short_name: "Studyswipe",
+    description:
+      "An app that allows you to study by gamifying your courses with flashcards.",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#ffffff",
+    theme_color: "#ffffff",
     icons: [
       {
-        src: '/icon-png.png',
-        sizes: '100x100',
-        type: 'image/png',
+        src: "/icon-png.png",
+        sizes: "100x100",
+        type: "image/png",
       },
     ],
-  }
+  };
 }
