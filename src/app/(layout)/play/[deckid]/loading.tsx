ad485b9af7355a48a147a36071b78cc14a434fed
bengaudry <bengaudry@outlook.fr>
2025-01-21 19:21:48 +0100

    Fix: Loader

--- src/app/(layout)/play/[deckid]/loading.tsx
+++ src/app/(layout)/play/[deckid]/loading.tsx
@@ -1,7 +1,7 @@
-import { CardsDisplayer } from "@/components/pages/play/CardsDisplayer";
+import { Spinner } from "@nextui-org/react";
 
 export default function Loader () {
     return <div className="grid place-content-center w-full h-full">
-        <CardsDisplayer deckCards={undefined} deckTheme="neutral" />
+        <Spinner />
     </div>
 }
