97b310eaaf18c8eb7d1e6eb52ab0f5630c64f5df
bengaudry <bengaudry@outlook.fr>
2025-05-15 13:17:29 +0200

    Styles: Better responsivness on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { Check, Plus } from "react-feather";
 export default function LandingPage() {
   return (
     <>
-      <section className="relative w-full h-[80vh] p-8 flex items-center justify-center">
+      <section className="relative w-full h-[80vh] p-2 sm:p-4 md:p-8 flex items-center justify-center">
         <div className="flex flex-col items-center justify-center rounded-3xl border dark:border-neutral-700 flex-1 w-full h-full max-w-screen-2xl dotted-bg">
           <h1 className="text-center text-secondary-950 text-4xl font-semibold sm:text-5xl md:text-6xl md:font-bold lg:text-7xl">
             Learn your course
