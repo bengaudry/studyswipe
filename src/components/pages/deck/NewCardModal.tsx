0e1a41e59e21a370716bd7d5774d3f034a83db46
bengaudry <bengaudry@outlook.fr>
2025-05-17 15:00:35 +0200

    Fix: Added apple photo format to filetypes for ai gen

--- src/components/pages/deck/NewCardModal.tsx
+++ src/components/pages/deck/NewCardModal.tsx
@@ -201,7 +201,7 @@ export function AiPromptModal({
 
   const handleChange = (file: File | null) => {
     setFile(file);
-    console.log(file?.name);
+    alert("File " + file?.name + file?.type);
   };
 
 
