cb33f8ca831ac52934b3299f0d58b67c3e8907e9
bengaudry <bengaudry@outlook.fr>
2026-04-06 17:27:54 +0200

    fix: cas support

--- src/app/api/cas-redirect/route.ts
+++ src/app/api/cas-redirect/route.ts
@@ -8,38 +8,52 @@ export const GET = async (req: NextRequest) => {
 
     const serviceTicket = params.get("st")
     if (!serviceTicket) {
-        return NextResponse.json({ error: "Bad request" }, { status: 400 });
+        return NextResponse.json({error: "Bad request"}, {status: 400});
     }
 
     const response = await axios.get(`https://cas.bengaudry.dev/api/v1/validate-ticket?st=${serviceTicket}&serviceId=studyswipe}`)
     if (response.status !== 200) {
-        return NextResponse.json({ error: "Bad request" }, { status: 400 });
+        return NextResponse.json({error: "Bad request"}, {status: 400});
     }
 
-    const { valid, user } = response.data;
+    const {valid, user} = response.data;
     if (!valid) {
-        return NextResponse.json({ error: "Invalid ticket" }, { status: 400 });
+        return NextResponse.json({error: "Invalid ticket"}, {status: 400});
     }
 
-    const localUser = await prisma.user.findUnique({
-        where: { email: user.email },
-    })
-    if (!localUser) {
-        await prisma.casUser.create({
-            data: {
-                id: user.id,
-                email: user.email,
-                name: user.name,
-                surname: user.surname,
-                displayName: user.displayName,
-                profilePictureUrl: user.profilePictureUrl,
-                isEmailVerified: user.isEmailVerified,
-                casCreatedAt: user.createdAt,
-                plan: user.role === "ADMIN" ? "PREMIUM" : "FREE"
-            },
+    try {
+        const localUser = await prisma.user.findUnique({
+            where: {email: user.email},
         })
+        if (!localUser) {
+            try {
+                await prisma.casUser.create({
+                    data: {
+                        id: user.id,
+                        email: user.email,
+                        name: user.name,
+                        surname: user.surname,
+                        displayName: user.displayName,
+                        profilePictureUrl: user.profilePictureUrl,
+                        isEmailVerified: user.isEmailVerified,
+                        casCreatedAt: user.createdAt,
+                        plan: user.role === "ADMIN" ? "PREMIUM" : "FREE"
+                    },
+                })
+            } catch (err) {
+                console.error("Could not create user after cas redirection")
+                console.error(err)
+                redirect("/?err=create-user-failed")
+            }
+        }
+
+        // user already exists
+        const redirectUrl = `${process.env.NEXT_PUBLIC_APP_URL}/`;
+        redirect(redirectUrl)
+    } catch (err) {
+        console.error("Error while fetching user after cas redirection")
+        console.error(err)
+        redirect("/?err=fetch-user-failed")
     }
 
-    const redirectUrl = `${process.env.NEXT_PUBLIC_APP_URL}/`;
-    redirect(redirectUrl)
 }
